94a665afbbdeba5b3e7df24fdb26c7686c17bd73
ngweihow <ngweihow.r@gmail.com>
2019-04-20 14:07:06 +1000

    Insertion and printing works

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,18 @@ public class Main {
 
         Random random = new Random();
         Tree tree = new Tree(random.nextInt(MAX_TREE));
-        tree.initTree();
+        //tree.initTree();
+
+        // Test Search
+        Node root = new Node(6);
+        tree.setRoot(root);
+        tree.insertNode(null, root);
+        tree.insertNode(tree.getRoot(), new Node(7));
+        tree.insertNode(tree.getRoot(), new Node(8));
+        tree.insertNode(tree.getRoot(), new Node(3));
+        tree.insertNode(tree.getRoot(), new Node(5));
+        tree.printTree();
+        System.out.println(tree.treeSearch(root, new Node(9)));
 
 
     }
--- src/Tree.java
+++ src/Tree.java
@@ -1,33 +1,43 @@
 import java.util.Random;
 
 public class Tree {
-    Node root;
-    int depth;
-    int nodesNumber;
+    private Node root = null;
+    private int depth;
+    private int nodesNumber;
+
+    public static final int COUNT = 10;
 
     Tree(int nodesNumber){
         this.nodesNumber = nodesNumber;
     }
 
-    void insertNode(Node root, Node newNode){
+    Node insertNode(Node root, Node newNode){
         if(root == null) {
             root = newNode;
-            return;
+            return root;
         }
 
         if(newNode.val <= root.val) {
-            insertNode(root.left, newNode);
+            root.left = insertNode(root.left, newNode);
         } else {
-            insertNode(root.right, newNode);
+            root.right = insertNode(root.right, newNode);
         }
+
+        return root;
     }
 
     /**
      * Intialise the tree with random values of nodes.
      */
     void initTree(){
+        // Add a root first
+        Random random = new Random();
+        Node newRoot = new Node(random.nextInt(Main.MAX_NODE));
+        root = newRoot;
+        insertNode(this.root, newRoot);
+
+        // Generate the subsequent Nodes
         for(int i=0;i<nodesNumber;i++) {
-            Random random = new Random();
             Node newNode = new Node(random.nextInt(Main.MAX_NODE));
             insertNode(this.root, newNode);
             System.out.println(newNode.val);
@@ -49,5 +59,69 @@ public class Tree {
         } else {
             return treeSearch(root.right, query);
         }
+    }
+
+    /**
+     * Print the Binary Tree.
+     */
+    public void printTree() {
+        if(root != null) {
+            print2DUtil(root, 0);
+        }
+    }
+
+    /**
+     * Method to print out binary tree horizontally.
+     * Adapted from https://www.geeksforgeeks.org/print-binary-tree-2-dimensions/.
+     * @param root Root of the tree, set recursively along with traversal.
+     * @param space Space between the branches.
+     */
+    private static void print2DUtil(Node root, int space) {
+        // Base case
+        if (root == null) return;
+
+        // Increase distance between levels
+        space += COUNT;
+
+        // Process right child first
+        print2DUtil(root.right, space);
+
+        // Print current node after space
+        // count
+        System.out.print("\n");
+        for (int i = COUNT; i < space; i++)
+            System.out.print(" ");
+        System.out.print(root.val + "\n");
+
+        // Process left child
+        print2DUtil(root.left, space);
+    }
+
+    public Node deleteNode(Node root, Node toBeRemoved) {
+        if (root == null || toBeRemoved == null) return null;
+
+        if(root.val == toBeRemoved.val){
+            // If node to be deleted has no child
+            if(root.left == null && root.right == null) {
+                root = null;
+                return null;
+            }
+
+            // If node to be deleted has one child
+            
+        }
+
+
+    }
+
+
+    // Getters and setters
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    public Node getRoot() {
+        return this.root;
     }
 }
